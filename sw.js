7bfe53eb6689a3b45ee1a9b59f28ce41db0f3aa6
massei100 <soju1979@gmail.com>
2026-09-09 18:08:24 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='gallery-v1',A=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
